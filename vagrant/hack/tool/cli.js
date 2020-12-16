317f9ab67f8300b3cbe7981e23655d97707fc44c
igibek <koishybayev.igibek@gmail.com>
2020-12-16 12:56:39 -0500

    custom hack tool

---
+++ vagrant/hack/tool/cli.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+const { version, description } = require('./package.json');
+const { program } = require('commander')
+const shell = require('shelljs');
+const chalk = require('chalk');
+const fs = require('fs');
+const path = require('path');
+program
+    .version(version)
+    .description(description)
+    .requiredOption('-d, --domain <domain>', 'the seed domain name of the target')
+    .arguments('<project>')
+    .action(main)
+
+
+
+if (!shell.which('git')) {
+    console.log(chalk.red('FAIL:'), 'this program depends on git and gobuster to work');
+    process.exit(1);
+}
+
+program.parse(process.argv)
+
+function main(project) {
+    console.log(chalk.yellow('START:'), `automation for the project ${project}`);
+    let dir = path.resolve(`./${project}`);
+    if (fs.existsSync(dir)) {
+        
+    }
+}
---
+++ vagrant/hack/tool/h1.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+DIR=$HOME/Desktop
+PROJECT=$1
+
+echo $PROJECT $DIR
+
+# 1. create project directory
+
+# 2. gather intelligence 
