f4d28a0ce22922239222c6c18d84cbed221901f2
igibek <koishybayev.igibek@gmail.com>
2020-12-16 19:06:17 -0500

    updated provisioning scripts

--- vagrant/provision/recon.sh
+++ vagrant/provision/recon.sh
@@ -1,5 +1,14 @@
-printf "Installing gobuster"
+echo ">> Installing gobuster"
 go get github.com/OJ/gobuster
 
-printf "Installing subfinder"
+echo ">> Installing subfinder"
 go get github.com/projectdiscovery/subfinder/v2/cmd/subfinder
+
+echo ">> Installing aquatone"
+go get github.com/michenriksen/aquatone
+
+echo ">> Setting up the Hack"
+cd /vagrant/hack/tool/ 
+npm ci
+cd /vagrant
+sudo ln -sf /vagrant/hack/tool/cli.js /usr/local/bin/hack
--- vagrant/provision/requirements.sh
+++ vagrant/provision/requirements.sh
@@ -1,6 +1,12 @@
 apt update
+echo ">> Installing the Go-lang"
 apt install -y \
     golang-go
 echo 'export PATH="$PATH:$HOME/go/bin"' >> .zshrc
+
+# Node.js
+echo ">> Installing the Node.js"
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
 echo 'alias c=clear' >> .zshrc
-sudo ln -sf /vagrant/tool/cli.js /usr/local/bin/hack
